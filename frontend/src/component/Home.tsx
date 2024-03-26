5f43037bc93dc2655e816e8391022c6f39a44755
Fabio Liberto <fabio.liberto@ergon.ch>
2024-03-26 15:19:42 +0100

    feature/welcome-screen: implemented review feedback

--- frontend/src/component/Home.tsx
+++ frontend/src/component/Home.tsx
@@ -7,12 +7,12 @@ export default function Home() {
     const navigate = useNavigate();
 
     return (
-        <div>
+        <>
             <Header headerModel={HeaderModel.START}/>
             <div className={"flex justify-center"}>
                 <h1>Willkommen</h1>
                 <Button className={"absolute bottom-2"} onClick={() => navigate("/tickets")}>Tickets</Button>
             </div>
-        </div>
+        </>
     )
 }
