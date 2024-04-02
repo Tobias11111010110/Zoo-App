fba2e49c567d35a49ebc0dfd9bc3068b54198dfa
Jutarokun <jesseryan@bluewin.ch>
2024-04-02 11:17:19 +0200

    needed ticket saving in database

--- backend/database.js
+++ backend/database.js
@@ -19,6 +19,7 @@ async function validateUserCredentials(email) {
                 reject(err);
             } else {
                 resolve(row);
+                console.log('Row in database: ' + row);
             }
         });
     })
@@ -33,7 +34,7 @@ async function insertUser(email, password) {
             reject(error);
             return;
         }
-        const insertQuery = 'INSERT INTO User (email, Passwort) VALUES (?, ?)';
+        const insertQuery = 'INSERT INTO User (Email, Password) VALUES (?, ?)';
 
         db.run(insertQuery, [email, hashedPassword], (err) => {
             if (err) {
@@ -47,13 +48,12 @@ async function insertUser(email, password) {
 
 async function validateUserLogin(email, password) {
     const exists = await validateUserCredentials(email);
-    console.log(exists)
 
     return new Promise((resolve, reject) => {
         if (exists == undefined) {
-            resolve(false)
+            resolve(false);
         } else {
-            bcrypt.compare(password, exists.Passwort, (err, result) => {
+            bcrypt.compare(password, exists.Password, (err, result) => {
                 if (err) {
                     reject(err);
                 } else if (result) {
@@ -66,11 +66,53 @@ async function validateUserLogin(email, password) {
     })
 };
 
+async function test() {
+    return new Promise((resolve, reject) => {
+        const statement = 'SELECT * FROM User'
+        db.get(statement, (err, row) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(row)
+            }
+        })
+    })
+}
+
+async function insertTicket(type, price, duration, date, userID) {
+    const query = 'INSERT INTO Ticket (Type, Price, Duration, Date, fk_userID) VALUES (?,?,?,?,?)';
+    return new Promise((resolve, reject) => {
+        db.run(query, [type, price, duration, date, userID], (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(true);
+            }
+        })
+    })
+}
+
+async function getUserID(email) {
+    const query = 'SELECT * FROM User WHERE email = ?';
+    return new Promise((resolve, reject) => {
+        db.get(query, [email], (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(row.Id);
+            }
+        })
+    })
+}
+
 
 module.exports = {
     validateUserCredentials,
     insertUser,
-    validateUserLogin
+    validateUserLogin,
+    test,
+    insertTicket,
+    getUserID
 };
 
 
